2d4b94c0dd5a2684f96f171d89130a678dce0b2e
gajanan15 <gajanandesai97@gmail.com>
2020-01-30 15:26:24 +0530

    ADDED: function to determine options for player

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -4,10 +4,32 @@ echo "Welcome To Snake And Ladder Simulator"
 #constant
 NUMBER_OF_PLAYER=1
 START_POSITION=0
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+
+#variables
+currentPosition=$START_POSITION
 
 function rollingDice() {
 	getValue=$((RANDOM%6 + 1))
-	echo $getValue
+	echo "After Rolling Dice Number is : "$getValue
+	snakeLadderOrNoPlay
 }
 
+function snakeLadderOrNoPlay() {
+	position=$((RANDOM%3))
+	case $position in
+		$NO_PLAY)
+			currentPosition=$currentPosition
+			;;
+		$LADDER)
+			currentPosition=$(($currentPosition + $getValue))
+			;;
+		$SNAKE)
+			currentPosition=$(($currentPosition - $getValue))
+			;;
+	esac
+	echo "Current Position: "$currentPosition
+}
 rollingDice

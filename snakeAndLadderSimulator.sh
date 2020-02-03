806f38fbbdaa4777a79278c3f948c6006d54c9c6
gajanan15 <gajanandesai97@gmail.com>
2020-02-03 19:12:37 +0530

    MODIFIED: Terminating Condition In While

--- snakeAndLadderSimulator.sh
+++ snakeAndLadderSimulator.sh
@@ -2,8 +2,6 @@
 echo "Welcome To Snake And Ladder Simulator"
 
 #constant
-PLAYER_ONE=0
-PLAYER_TWO=1
 START_POSITION=0
 WINNING_POSITION=100
 NO_PLAY=0
@@ -12,72 +10,69 @@ SNAKE=2
 
 #variables
 playerCurrentPosition=$START_POSITION
-playerOnePosition=0
-playerTwoPosition=0
+playerOnePosition=$START_POSITION
+playerTwoPosition=$START_POSITION
 diceCount=0
 flag=0
 
 function rollingDice() {
 	rollValue=$((RANDOM%6+1))
 	((diceCount++))
-} 
+	checkForOption
+}
 
 function checkForOption() {
 	position=$((RANDOM%3))
-	playerCurrentPosition=$1
 	case $position in
 		$NO_PLAY)
 			playerCurrentPosition=$playerCurrentPosition
 			;;
 		$LADDER)
-			playerCurrentPosition=$(( $playerCurrentPosition + $2 ))
+			playerCurrentPosition=$(( $playerCurrentPosition + $rollValue ))
+			if [ $playerCurrentPosition -gt $WINNING_POSITION ]
+			then
+				playerCurrentPosition=$(( $playerCurrentPosition - $rollValue ))
+			fi
 			;;
 		$SNAKE)
-			playerCurrentPosition=$(( $playerCurrentPosition - $2 ))
+			playerCurrentPosition=$(( $playerCurrentPosition - $rollValue ))
+			if [ $playerCurrentPosition -lt $START_POSITION ]
+			then
+				playerCurrentPosition=$START_POSITION
+			fi
 			;;
 	esac
 	echo $playerCurrentPosition
 }
 
-function reachExactPosition() {
-	playerPosition=$1
-	if [ $playerPosition -lt 0 ]
-	then
-		playerPosition=$START_POSITION
-	fi
-	if [ $playerPosition -gt $WINNING_POSITION ]
-	then
-		playerPosition=$(( $playerPosition - $2 ))
-	fi
-	echo $playerPosition
-}
-
 function switchTurnsOfPlayers() {
-	while [[ $playerOnePosition -ne $WINNING_POSITION && $playerTwoPosition -ne $WINNING_POSITION ]]
+	while [[ $playerCurrentPosition -ne $WINNING_POSITION ]]
 	do
 		if [ $flag -eq 0 ]
 		then
+			playerCurrentPosition=$playerOnePosition
 			rollingDice
-			firstPlayerPosition=$( checkForOption $playerOnePosition $rollValue )
-			playerOnePosition=$( reachExactPosition $firstPlayerPosition $rollValue )
+			playerOnePosition=$playerCurrentPosition
 			flag=1
 		else
+			playerCurrentPosition=$playerTwoPosition
 			rollingDice
-			secondPlayerPosition=$( checkForOption $playerTwoPosition $rollValue )
-			playerTwoPosition=$( reachExactPosition $secondPlayerPosition $rollValue )
+			playerTwoPosition=$playerCurrentPosition
 			flag=0
 		fi
 	done
 	checkWinner
 }
+
 function checkWinner() {
 	if [ $playerOnePosition -eq $WINNING_POSITION ]
-   then
-      echo "Player one win"
-      echo "Number Of Time Dice Tossed Player one: " $(($diceCount + 1))
-   else
-      echo "Player Two win"
-      echo "Number Of Time Dice Tossed Player Two: " $diceCount
-   fi
+	then
+		echo "Player one win"
+		echo "Number Of Time Dice Tossed Player one: " $(($diceCount + 1))
+	else
+		echo "Player Two win"
+		echo "Number Of Time Dice Tossed Player Two: " $diceCount
+	fi
 }
+
 switchTurnsOfPlayers
